8089f5eff325308204989ae60cd4beff44f50068
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-04-10 13:57:32 -0600

    Added the ShapeCanvas class and code to a couple of the methods inside of it.

---
+++ src/art/view/ShapeCanvas.java
@@ -0,0 +1,75 @@
+package art.view;
+
+import java.awt.Polygon;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class ShapeCanvas eArrayList<E>anel 
+{
+	private ArrayList<Polygon> triangleList;
+	private ArrayList<Polygon> polygonList;
+	private ArrayList<Ellips2D> ellipseList;
+	private ArrayList<Rectangle> rectangleList;
+	private ArtController app;
+	
+	private BufferedImage canvasImage;
+	
+	public ShapeCanvas(ArtController app)
+	{
+		super();
+		this.app = app;
+		riangleList = new ArrayList<Polygon>();
+		polygonList = new ArrayList<Polygon>();
+		ellipseList = new ArrayList<Ellipse2D>();
+		rectangleList = new ArrayList<Rectangle>();
+		
+		canvasImage = new BufferedImage(600, 600, BufferedImage.TYPE_INT_ARGB);
+		this.setMinimumSize(new Dimension(600, 600));
+		this.setPreferredSize(new Dimension(600, 600));
+		this.setMaximumSize(getPreferredSize());
+	}
+	
+	public void addShape(shape current)
+	{
+		if (current instanceof Polygon)
+		{
+			if(((Polygon)current).xpoints.length == 3)
+			{
+				triangleList.add((Polygon) current);
+			}
+			else
+			{
+				polygonList.add((Polygon)current);
+			}
+		}
+		else if (current instanceof Ellipse2D)
+		{
+			ellipseList.add((Ellipse2D)current);
+		}
+		else
+		{
+			rectangleList.add((Rectangle)current):
+		}
+		updateImage();
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public void changeBackround()
+	{
+		
+	}
+	
+	public void save()
+	{
+		
+	}
+	
+	
+
+}
