84b0849c61c03282f4c54fe5573382542f3a38a2
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2018-04-26 14:04:52 -0600

    added the controller and runner code

--- src/art/controller/Controller.java
+++ src/art/controller/Controller.java
@@ -1,5 +1,30 @@
 package art.controller;
 
-public class Controller {
+import java.awt.Component;
+import javax.swing.JOptionPane;
+import art.view.ArtFrame;
 
+public class Controller 
+{
+	private ArtFrame appFrame;
+	
+	public Controller()
+	{
+		this.appFrame = new ArtFrame(this);
+	}
+	
+	public void start()
+	{
+		JOptionPane.showMessageDialog(appFrame,  "Ya Like Art?");
+	}
+	
+	public void handleErrors(Exception error)
+	{
+		JOptionPane.showMessageDialog(appFrame, error.getMessage());
+	}
+	
+	public ArtFrame getFrame()
+	{
+		return appFrame;
+	}
 }
